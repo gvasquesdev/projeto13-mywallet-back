3dab3754c2b8445885981b9ab3e958bb10ae64f5
Gustavo Vasques <gvasques.dev@gmail.com>
2022-07-04 12:24:31 -0300

    Rota signin + validação por token implementados e testados

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express, { json } from 'express';
-import chalk from 'chalk';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import express, { json } from "express";
+import chalk from "chalk";
+import cors from "cors";
+import joi from "joi";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
 
 import db from "./dbStrategy/db.js"
-import userSchema from "./schemas/userSchema.js"
+import { userSchema }  from "./schemas/userSchema.js"
 
 dotenv.config();
 const app = express();
@@ -16,7 +19,6 @@ app.use(json(), cors());
 
 // signup e signin
 app.post('/sign-up', async (req,res) => {
-      
      const {error} = userSchema.validate(req.body, {abortEarly: false});
 
      try {
@@ -36,6 +38,32 @@ app.post('/sign-up', async (req,res) => {
         return res.sendStatus(500);
       }
 })
+
+app.post("/signin", async (request, response) => {
+
+  const signInSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+  });
+  const { error } = signInSchema.validate(request.body, {abortEarly: false});
+    if(error){
+        return response.sendStatus(422)
+    }
+
+  try {
+    const user = await db.collection("users").findOne({email: request.body.email})
+    if(!user) return response.sendStatus(404);
+    if(user && bcrypt.compareSync(request.body.password, user.password)){
+      const token = uuid();
+      await db.collection("sessions").insertOne({token, userId: user._id})
+      response.send({token, name: user.name});
+    } else {
+      return response.sendStatus(401)
+    }
+  } catch {
+    return response.sendStatus(500);
+  }
+});
 
 
 const port = process.env.PORT || 5000;
